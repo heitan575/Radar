41a97bf4f827b2ad8ed5bd2caa56aec6e0a32b2c
黑炭 <362983772@qq.com>
2017-06-26 15:48:15 +0800

    create project

---
+++ Radar/Radar.h
@@ -0,0 +1,13 @@
+//
+//  Radar.h
+//  Radar
+//
+//  Created by 曾惠强 on 2017/6/26.
+//  Copyright © 2017年 曾惠强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Radar : UIView
+
+@end
---
+++ Radar/Radar.m
@@ -0,0 +1,21 @@
+//
+//  Radar.m
+//  Radar
+//
+//  Created by 曾惠强 on 2017/6/26.
+//  Copyright © 2017年 曾惠强. All rights reserved.
+//
+
+#import "Radar.h"
+
+@implementation Radar
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
